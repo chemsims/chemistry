dc0cd7a751fd6ac7ad97b4e5c4eae9f95d8a04dd
omar-f <o.fahmy@outlook.com>
2020-12-28 14:06:16 +0000

    Added documentation to QuizQuestion struct

--- reactions-app/model/quiz/QuizQuestion.swift
+++ reactions-app/model/quiz/QuizQuestion.swift
@@ -5,6 +5,73 @@
 
 import Foundation
 
+/// Definition of a quiz question.
+///
+/// A correct answer, and a number of incorrect answers are provided. Note that any number of incorrect answers can be provided, however they must
+/// not exceed the maximum allowable number of options (as defined by the length of QuizOption).
+///
+/// Usage
+/// =====
+/// The simplest way to create a quiz question is to use String literals, since most parameters can be expressed as a String.
+/// For example,
+/// ```
+/// QuizQuestion(
+///   question: "What is 1 + 1?",
+///   correctAnswer: "2",
+///   otherAnswers: ["1", "3", "4"],
+///   explanation: "1 + 1 is always equal to 2",
+///   difficulty: .easy
+/// )
+/// ```
+///
+/// Answer Order
+/// ============
+/// Answers are displayed in a random order by default. It is possible to assign an explicit position to an answer.
+/// Two answers should not share the same position, and a runtime assertion will prevent this.
+///
+/// For example,
+/// ```
+/// QuizQuestion(
+///     question: "Which numbers are less than 5?",
+///     correctAnswer: QuizAnswer(answer: "All of the above", explanation: nil, position: QuizOption.D),
+///     otherAnswers: ["1", "2", "3"],
+///     explanation: "1, 2 and 3 are all less than 5",
+///     difficulty: .easy
+/// )
+/// ```
+///
+/// Explanations
+/// ============
+/// There are two types of explanation available.
+///  1. Those associated with the entire question
+///  2. Those associated with a particular answer
+///
+/// Both types of explanation are optional, and any combination can be provided.
+///
+/// Below is an example using a single explanation for the entire question.
+/// ```
+/// QuizQuestion(
+///     question: "What is tallest mountain in the world?",
+///     correctAnswer: "Mount Everest",
+///     otherAnswers: ["Mount Blanc", "Mount Fuji"],
+///     explanation: "Mount Everest is 8,849m tall, which is the tallest in the world.",
+///     difficulty: .medium
+/// )
+/// ```
+///
+/// Below is an example with a different explanation for each answer
+/// ```
+/// QuizQuestion(
+///     question: "What is the largest country in the world?",
+///     correctAnswer: QuizAnswer("Russia", "The area of Russia is 17.1 million square km which is the largest in the world"),
+///     otherAnswers: [
+///         QuizAnswer("Canada", "The area of Canada is 10 million square km"),
+///         QuizAnswer("USA", "The area of USA is 9.8 million square km")
+///     ],
+///     explanation: nil,
+///     difficulty: .hard
+/// )
+/// ```
 struct QuizQuestion {
     let question: TextLine
     let correctAnswer: QuizAnswer
