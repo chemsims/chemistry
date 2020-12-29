4024ae0e35a42d8d4faca58f621df33c6f69e20a
omar-f <o.fahmy@outlook.com>
2020-12-29 16:22:33 +0000

    Added a size adjusted struct to keep array elements within specific bounds

---
+++ reactions-app/util/SizeAdjuster.swift
@@ -0,0 +1,64 @@
+//
+// Reactions App
+//
+  
+
+import Foundation
+
+struct SizeAdjuster {
+    /// Returns a new array where each element is at each `minElementSize`, and the sum is
+    /// less than or equal to `maximumSum`
+    static func adjust(
+        sizes: [Double],
+        minElementSize: Double,
+        maximumSum: Double
+    ) -> [Double] {
+        sizes
+            .withMinElementSize(minElementSize)
+            .withSumBelow(limit: maximumSum, minSize: minElementSize)
+    }
+}
+
+fileprivate extension Array where Element == Double {
+
+    func withMinElementSize(_ minSize: Double) -> [Double] {
+        self.map { size in
+            size < minSize ? minSize : size
+        }
+    }
+
+    func withSumBelow(
+        limit: Double,
+        minSize: Double
+    ) -> [Double] {
+        let surplus = sum - limit
+        if (surplus <= 0) {
+            return self
+        }
+
+        let sortedElements = self.enumerated().sorted(by: {(a, b) in a.element < b.element })
+
+        var result = self
+        sortedElements.forEach { entry in
+            let remainingElements = sortedElements[entry.offset...]
+            let remainingSum = remainingElements.reduce(0) { $0 + $1.element }
+
+            let previousElements = (0..<entry.offset).map { i in
+                result[i]
+            }
+            let previousSum = previousElements.sum
+
+            let desiredScale = (limit - previousSum) / remainingSum
+            let idealDelta = (1 - desiredScale) * entry.element
+            let maxDelta = entry.element - minSize
+            result[entry.offset] = entry.element - Swift.min(maxDelta, idealDelta)
+        }
+        return result
+    }
+}
+
+fileprivate extension Sequence where Element == Double {
+    var sum: Double {
+        reduce(0) { $0 + $1 }
+    }
+}
---
+++ reactions-appTests/SizeAdjusterTests.swift
@@ -0,0 +1,72 @@
+//
+// Reactions App
+//
+  
+
+import XCTest
+@testable import reactions_app
+
+class SizeAdjusterTests: XCTestCase {
+
+
+    func testIncreasingSmallElement() {
+        let adjusted1 = SizeAdjuster.adjust(
+            sizes: [1, 100],
+            minElementSize: 5,
+            maximumSum: 105
+        )
+        let adjusted2 = SizeAdjuster.adjust(
+            sizes: [100, 1],
+            minElementSize: 5,
+            maximumSum: 105
+        )
+        XCTAssertEqual(adjusted1, [5, 100])
+        XCTAssertEqual(adjusted2, [100, 5])
+    }
+
+    func testRemainingWithinBoundsWithTwoLargeElementsOfTheSameSize() {
+        let adjusted = SizeAdjuster.adjust(
+            sizes: [100, 100],
+            minElementSize: 1,
+            maximumSum: 100
+        )
+        XCTAssertEqual(adjusted, [50, 50])
+    }
+
+    func testRemainingWithinBoundsWithTwoLargeElementsOfDifferentSize() {
+        let adjusted = SizeAdjuster.adjust(
+            sizes: [50, 200],
+            minElementSize: 0,
+            maximumSum: 50
+        )
+        XCTAssertEqual(adjusted, [10, 40])
+    }
+
+    func testRemainingWithinBoundsWithASmallElementAndALargeElement() {
+        let adjusted = SizeAdjuster.adjust(
+            sizes: [5, 100],
+            minElementSize: 5,
+            maximumSum: 50
+        )
+        assertArrayEquals(adjusted, [5, 45])
+    }
+
+    func testIncreasingMinSizeAndRemainingWithinBoundsForMultipleElements() {
+        let adjusted = SizeAdjuster.adjust(
+            sizes: [1, 10, 60, 100, 400, 1000],
+            minElementSize: 50,
+            maximumSum: 400
+        )
+        let expectedPenultimate = (200.0 / 1400.0) * 400.0
+        let expectedFinal = 400 - 200 - expectedPenultimate
+        assertArrayEquals(adjusted, [50, 50, 50, 50, expectedPenultimate, expectedFinal])
+    }
+
+    private func assertArrayEquals<T: BinaryFloatingPoint>(_ lhs: Array<T>, _ rhs: Array<T>, _ accuracy: T = 0.0001) {
+        XCTAssertEqual(lhs.count, rhs.count)
+        (0..<lhs.count).forEach { i in
+            XCTAssertEqual(lhs[i], rhs[i], accuracy: accuracy)
+        }
+    }
+
+}
