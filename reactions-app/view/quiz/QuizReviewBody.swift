b134ff2476c38f918223f26080246037bf304181
omar-f <o.fahmy@outlook.com>
2021-01-05 21:09:06 +0000

    Refactored quiz review screen by moving the review card into a separate view

--- reactions-app/view/quiz/QuizReviewBody.swift
+++ reactions-app/view/quiz/QuizReviewBody.swift
@@ -10,8 +10,6 @@ struct QuizReviewBody: View {
     let settings: QuizLayoutSettings
     @ObservedObject var model: QuizViewModel
 
-    @State private var expandedExplanations = Set<Int>()
-
     var body: some View {
         HStack(spacing: 0) {
             ScrollView {
@@ -70,7 +68,27 @@ struct QuizReviewBody: View {
         let selectedOption = model.selectedOption(index: index)
         let isCorrect = selectedOption == question.correctOption
 
-        return ZStack(alignment: .leading) {
+        return QuestionReviewCard(
+            question: question,
+            selectedOption: selectedOption ?? .A,
+            isCorrect: isCorrect,
+            settings: settings
+        )
+    }
+}
+
+fileprivate struct QuestionReviewCard: View {
+
+    let question: QuizQuestionDisplay
+    let selectedOption: QuizOption
+    let isCorrect: Bool
+    let settings: QuizLayoutSettings
+
+
+    @State private var explanationIsExpanded: Bool = false
+
+    var body: some View {
+        ZStack(alignment: .leading) {
             VStack(alignment: .leading, spacing: 10) {
                 HStack(spacing: 0) {
                     Spacer()
@@ -81,7 +99,7 @@ struct QuizReviewBody: View {
                 )
 
                 optionLine(
-                    option: selectedOption!,
+                    option: selectedOption,
                     topLine: "Your answer",
                     question: question
                 )
@@ -95,11 +113,11 @@ struct QuizReviewBody: View {
                 }
 
                 VStack(alignment: .leading, spacing: 0) {
-                    Button(action: { handleExplanationPress(index: index) }) {
-                        Text(expandedExplanations.contains(index) ? "Hide Explanation" : "Show Explanation")
+                    Button(action: handleExplanationPress) {
+                        Text(explanationIsExpanded ? "Hide Explanation" : "Show Explanation")
                             .font(.system(size: settings.questionFontSize))
                     }
-                    if (expandedExplanations.contains(index)) {
+                    if (explanationIsExpanded) {
                         TextLinesView(
                             lines: question.longExplanation,
                             fontSize: settings.questionFontSize
@@ -113,33 +131,7 @@ struct QuizReviewBody: View {
         }
         .padding(.horizontal, 2)
         .font(.system(size: settings.questionFontSize))
-        .background(
-            reviewBackground(isCorrect: isCorrect)
-        )
-    }
-
-    private func reviewBackground(isCorrect: Bool) -> some View {
-        ZStack(alignment: .topTrailing) {
-            RoundedRectangle(
-                cornerRadius: settings.progressCornerRadius
-            )
-            .foregroundColor(.white)
-            .shadow(color: borderColor(isCorrect: isCorrect), radius: 3, x: 0, y: 0)
-
-            RoundedRectangle(
-                cornerRadius: settings.progressCornerRadius
-            )
-            .strokeBorder(lineWidth: 2)
-            .foregroundColor(borderColor(isCorrect: isCorrect))
-
-            QuizAnswerIconOverlay(isCorrect: isCorrect)
-                .frame(width: 30, height: 30)
-                .offset(x: 10, y: -10)
-        }
-    }
-
-    private func borderColor(isCorrect: Bool) -> Color {
-        isCorrect ? Styling.quizAnswerCorrectBorder : Styling.quizAnswerIncorrectBorder
+        .background(reviewBackground)
     }
 
     private func optionLine(
@@ -165,31 +157,35 @@ struct QuizReviewBody: View {
         .fixedSize(horizontal: false, vertical: true)
     }
 
-    private func incorrectAnswer(
-        question: QuizQuestionDisplay,
-        selectedOption: QuizOption?
-    ) -> some View {
-        let selectedAnswer = selectedOption.flatMap { question.options[$0]?.answer } ?? ""
-        return VStack(alignment: .leading) {
-            HStack {
-                Text("Your answer")
-                TextLinesView(
-                    line: selectedAnswer,
-                    fontSize: settings.fontSize
-                )
-                .foregroundColor(.orangeAccent)
-            }
+    private var reviewBackground: some View {
+        ZStack(alignment: .topTrailing) {
+            RoundedRectangle(
+                cornerRadius: settings.progressCornerRadius
+            )
+            .foregroundColor(.white)
+            .shadow(color: borderColor, radius: 3, x: 0, y: 0)
+
+            RoundedRectangle(
+                cornerRadius: settings.progressCornerRadius
+            )
+            .strokeBorder(lineWidth: 2)
+            .foregroundColor(borderColor)
+
+            QuizAnswerIconOverlay(isCorrect: isCorrect)
+                .frame(width: 30, height: 30)
+                .offset(x: 10, y: -10)
         }
     }
 
-    private func handleExplanationPress(index: Int) {
+    private var borderColor: Color {
+        isCorrect ? Styling.quizAnswerCorrectBorder : Styling.quizAnswerIncorrectBorder
+    }
+
+    private func handleExplanationPress() {
         withAnimation(.easeOut(duration: 0.5)) {
-            if (expandedExplanations.contains(index)) {
-                expandedExplanations.remove(index)
-            } else {
-                expandedExplanations.insert(index)
-            }
+            explanationIsExpanded.toggle()
         }
     }
-}
 
+
+}
