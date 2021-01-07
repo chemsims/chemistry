54c0f0ddb9d23341c90538f6fd0bc18f133a45af
omar-f <o.fahmy@outlook.com>
2021-01-07 22:31:10 +0000

    Removed unused functions from first/second order reaction view model

--- reactions-app/viewmodels/reactions/FirstOrderReactionViewModel.swift
+++ reactions-app/viewmodels/reactions/FirstOrderReactionViewModel.swift
@@ -7,19 +7,6 @@ import SwiftUI
 
 class FirstOrderReactionViewModel: ZeroOrderReactionViewModel {
 
-    func generateEquation(
-        c1: CGFloat,
-        c2: CGFloat,
-        t1: CGFloat,
-        t2: CGFloat
-    ) -> ConcentrationEquation {
-        FirstOrderConcentration(
-            c1: c1,
-            c2: c2,
-            time: t2
-        )
-    }
-
     var logAEquation: Equation? {
         input.concentrationA.map { LogEquation(underlying: $0) }
     }
--- reactions-app/viewmodels/reactions/SecondOrderReactionViewModel.swift
+++ reactions-app/viewmodels/reactions/SecondOrderReactionViewModel.swift
@@ -7,19 +7,6 @@ import CoreGraphics
 
 class SecondOrderReactionViewModel: ZeroOrderReactionViewModel {
 
-    func generateEquation(
-        c1: CGFloat,
-        c2: CGFloat,
-        t1: CGFloat,
-        t2: CGFloat
-    ) -> ConcentrationEquation {
-        return SecondOrderConcentration(
-            c1: c1,
-            c2: c2,
-            time: t2
-        )
-    }
-
     var inverseAEquation: Equation? {
         input.concentrationA.map { InverseEquation(underlying: $0) }
     }
