c1f9510e89d66fca36ff0276d503732f4794bb94
omar-f <o.fahmy@outlook.com>
2021-01-09 22:18:38 +0000

    Re-added badge animation when selecting a quiz answer

--- reactions-app/view/quiz/QuizQuestionBody.swift
+++ reactions-app/view/quiz/QuizQuestionBody.swift
@@ -11,7 +11,6 @@ struct QuizQuestionsBody: View {
     @ObservedObject var model: QuizViewModel
 
     @State private var badgeScale: CGFloat = 1
-    @Environment(\.accessibilityReduceMotion) private var reduceMotion
 
     var body: some View {
         ScrollView {
@@ -81,28 +80,46 @@ struct QuizQuestionsBody: View {
     }
 
     private func answerWithExplanation(option: QuizOption) -> some View {
+        QuizAnswerOption(
+            model: model,
+            settings: settings,
+            option: option
+        )
+    }
+
+}
+
+fileprivate struct QuizAnswerOption: View {
+
+    @ObservedObject var model: QuizViewModel
+    let settings: QuizLayoutSettings
+    let option: QuizOption
+
+    @Environment(\.accessibilityReduceMotion) private var reduceMotion
+    @State private var badgeScale: CGFloat = 1
+
+    var body: some View {
         VStack(spacing: 0) {
-            answer(option: option)
-            if (model.showExplanation &&
-                    model.currentQuestion.hasExplanation(option: option)
+            answer
+            if (model.showExplanation && model.currentQuestion.hasExplanation(option: option)
             ) {
-                optionExplanation(option: option)
+                explanation
             }
         }
     }
 
-    private func answer(option: QuizOption) -> some View {
+    private var answer: some View {
         ZStack {
             Group {
                 RoundedRectangle(cornerRadius: settings.progressCornerRadius)
-                    .foregroundColor(answerBackground(option: option))
+                    .foregroundColor(answerBackground)
                     .shadow(radius: 3)
 
-                if (styleCorrect(option) || styleIncorrect(option)) {
+                if (styleCorrect || styleIncorrect) {
                     RoundedRectangle(cornerRadius: settings.progressCornerRadius)
                         .strokeBorder(lineWidth: settings.activeLineWidth)
-                        .foregroundColor(answerBorder(option: option))
-                        .overlay(overlay(option: option), alignment: .topTrailing)
+                        .foregroundColor(answerBorder)
+                        .overlay(overlay, alignment: .topTrailing)
                 }
 
                 TextLinesView(
@@ -113,12 +130,12 @@ struct QuizQuestionsBody: View {
                 .padding()
                 .minimumScaleFactor(0.5)
             }
-            .onTapGesture(perform: { handleAnswer(option: option) })
+            .onTapGesture(perform: { handleAnswer() })
         }
         .fixedSize(horizontal: false, vertical: true)
     }
 
-    private func optionExplanation(option: QuizOption) -> some View {
+    private var explanation: some View {
         let explanation = model.currentQuestion.options[option]?.explanation
         return TextLinesView(
                 line: explanation?.italic() ?? "",
@@ -128,12 +145,12 @@ struct QuizQuestionsBody: View {
         .fixedSize(horizontal: false, vertical: true)
     }
 
-    private func overlay(option: QuizOption) -> some View {
+    private var overlay: some View {
         ZStack {
-            if (styleCorrect(option) || styleIncorrect(option)) {
-                QuizAnswerIconOverlay(isCorrect: styleCorrect(option))
+            if (styleCorrect || styleIncorrect) {
+                QuizAnswerIconOverlay(isCorrect: styleCorrect)
                     .scaleEffect(badgeScale)
-//                    .onAppear(perform: runBadgeAnimation)
+                    .onAppear(perform: runBadgeAnimation)
             }
         }
         .frame(width: settings.navSize, height: settings.navSize)
@@ -141,39 +158,39 @@ struct QuizQuestionsBody: View {
         .id("\(model.questionIndex)-\(option.hashValue)")
     }
 
-//    private func runBadgeAnimation() {
-//        guard !reduceMotion else {
-//            return
-//        }
-//        if (model.selectedAnswer == model.correctOption) {
-//            badgeScale = 1
-//            withAnimation(.easeOut(duration: 0.35)) {
-//                badgeScale = 1.2
-//            }
-//            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(350)) {
-//                withAnimation(.easeInOut(duration: 0.25)) {
-//                    badgeScale = 1
-//                }
-//            }
-//        } else {
-//            badgeScale = 0.75
-//            withAnimation(.spring(response: 0.4, dampingFraction: 0.5)) {
-//                badgeScale = 1
-//            }
-//        }
-//    }
+    private func runBadgeAnimation() {
+        guard !reduceMotion, model.selectedAnswer?.allAnswers.contains(option) ?? false else {
+            return
+        }
+        if (option == model.correctOption) {
+            badgeScale = 1
+            withAnimation(.easeOut(duration: 0.35)) {
+                badgeScale = 1.2
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(350)) {
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    badgeScale = 1
+                }
+            }
+        } else {
+            badgeScale = 0.75
+            withAnimation(.spring(response: 0.4, dampingFraction: 0.5)) {
+                badgeScale = 1
+            }
+        }
+    }
 
-    private func handleAnswer(option: QuizOption) {
+    private func handleAnswer() {
         guard !model.hasSelectedCorrectOption else {
             return
         }
         model.answer(option: option)
     }
 
-    private func answerBackground(option: QuizOption) -> Color {
-        if (styleCorrect(option)) {
+    private var answerBackground: Color {
+        if (styleCorrect) {
             return Styling.Quiz.correctAnswerBackground
-        } else if (styleIncorrect(option)) {
+        } else if (styleIncorrect) {
             return Styling.Quiz.wrongAnswerBackground
         } else if (model.hasSelectedCorrectOption) {
             return Styling.Quiz.disabledAnswerBackground
@@ -181,20 +198,20 @@ struct QuizQuestionsBody: View {
         return Styling.Quiz.answerBackground
     }
 
-    private func answerBorder(option: QuizOption) -> Color {
-        if (styleCorrect(option)) {
+    private var answerBorder: Color {
+        if (styleCorrect) {
             return Styling.Quiz.correctAnswerBorder
-        } else if (styleIncorrect(option)) {
+        } else if (styleIncorrect) {
             return Styling.Quiz.wrongAnswerBorder
         }
         return Styling.Quiz.answerBackground
     }
 
-    private func styleCorrect(_ option: QuizOption) -> Bool {
+    private var styleCorrect: Bool {
         option == model.correctOption && model.hasSelectedCorrectOption
     }
 
-    private func styleIncorrect(_ option: QuizOption) -> Bool {
+    private var styleIncorrect: Bool {
         let isIncorrect = model.correctOption != option
         let isSelected = model.selectedAnswer?.allAnswers.contains(option) ?? false
         return isIncorrect && isSelected
