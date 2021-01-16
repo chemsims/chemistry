0b260af55eec6801369977973d19fc79fe027a04
omar-f <o.fahmy@outlook.com>
2021-01-16 19:38:47 +0000

    Added simple test for reaction comparison view model next is not applied while reactions have not been identified

---
+++ reactions-appTests/ReactionComparisonViewModelTests.swift
@@ -0,0 +1,37 @@
+//
+// Reactions App
+//
+  
+
+import XCTest
+@testable import reactions_app
+
+class ReactionComparisonViewModelTests: XCTestCase {
+
+    func testCannotProgressNextUntilReactionsHaveBeenCorrectlyIdentifiedOnce() {
+        let persistence = InMemoryReactionInputPersistence()
+        let model = ReactionComparisonViewModel(persistence: persistence)
+        let navigation = ReactionComparisonNavigationViewModel.model(reaction: model)
+        model.navigation = navigation
+
+        navigation.nextUntil(\.canDragOrders)
+
+        model.next()
+        XCTAssertTrue(model.canDragOrders)
+        XCTAssertFalse(model.reactionHasEnded)
+
+        model.addToCorrectSelection(order: .Zero)
+        model.addToCorrectSelection(order: .First)
+        model.addToCorrectSelection(order: .Second)
+
+        model.next()
+        XCTAssertTrue(model.reactionHasEnded)
+
+        model.back()
+        XCTAssertFalse(model.reactionHasEnded)
+
+        model.next()
+        XCTAssertTrue(model.reactionHasEnded)
+    }
+
+}
