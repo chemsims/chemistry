fffea0e420ae1df23f71e51a1d992c675f504527
omar-f <o.fahmy@outlook.com>
2021-02-23 13:18:23 +0400

    WIP adding aqueous reaction screen

---
+++ reactions-equilibrium/view/aqueous/AqueousReactionScreen.swift
@@ -0,0 +1,18 @@
+//
+// Reactions App
+//
+
+
+import SwiftUI
+
+struct AqueousReactionScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AqueousReactionScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AqueousReactionScreen()
+    }
+}
