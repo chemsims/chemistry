26941c1b87339e04746c381e1b2cea3225928034
omar-f <o.fahmy@outlook.com>
2021-02-27 20:53:56 +0400

    Added view for an animating pile of molecules

---
+++ reactions-equilibrium/view/scales/MoleculeScales.swift
@@ -0,0 +1,18 @@
+//
+// Reactions App
+//
+
+
+import SwiftUI
+
+struct MoleculeScales: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MoleculeScales_Previews: PreviewProvider {
+    static var previews: some View {
+        MoleculeScales()
+    }
+}
