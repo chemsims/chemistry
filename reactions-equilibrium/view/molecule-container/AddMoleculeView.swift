f3bab7265b4d7c6b7d8a78f09b286085974f7523
omar-f <o.fahmy@outlook.com>
2021-03-09 16:46:13 +0400

    EA-19: Made the molecule emit frequency depend on shaking speed

--- reactions-equilibrium/view/molecule-container/AddMoleculeView.swift
+++ reactions-equilibrium/view/molecule-container/AddMoleculeView.swift
@@ -3,6 +3,7 @@
 //
 
 import SwiftUI
+import ReactionsCore
 
 struct AddMoleculesView: View {
 
@@ -124,7 +125,7 @@ private struct AddMoleculeContainerView: View {
                     x: initialLocation.x + offset.width,
                     y: initialLocation.y + offset.height
                 )
-                model.add(at: effectivePosition, to: startOfWater + (moleculeSize / 2))
+                model.add(at: effectivePosition, to: startOfWater + (moleculeSize / 2), time: drag.time)
             }.onEnded { _ in
                 withAnimation(.spring()) {
                     offset = .zero
@@ -187,21 +188,25 @@ class AddingSingleMoleculeViewModel: ObservableObject {
     @Published var molecules = [FallingMolecule]()
 
     private var lastDrop: Date?
-    private let timeBetweenDrops = 0.15
     private let velocity = 200.0 // pts per second
+
+    @Published var lastAddAttempt: (CGPoint, Date)?
 
     /// Adds a molecule at the start position, and animates the Y value to endY
     func add(
         at startPosition: CGPoint,
-        to endY: CGFloat
+        to endY: CGFloat,
+        time: Date
     ) {
-        print("can add? \(canAddMolecule())")
+
+        let timeBetweenDrops = getTimeIntervalBetweenDrops(startPosition: startPosition, currentTime: time)
         guard canAddMolecule() else {
             return
         }
-        guard lastDrop == nil || Date().timeIntervalSince(lastDrop!) >= timeBetweenDrops else {
+        guard lastDrop == nil || Date().timeIntervalSince(lastDrop!) >= Double(timeBetweenDrops) else {
             return
         }
+        print(timeBetweenDrops)
         lastDrop = Date()
 
         let molecule = FallingMolecule(position: startPosition)
@@ -225,12 +230,74 @@ class AddingSingleMoleculeViewModel: ObservableObject {
         addMolecules(count)
     }
 
+
+    private func handleVelocityCheck(position: CGPoint) {
+        let now = Date()
+        if let lastAttempt = lastAddAttempt {
+            let dx = abs(position.x - lastAttempt.0.x)
+            let dy = abs(position.y - lastAttempt.0.y)
+            let magChange = sqrt(pow(dx, 2) + pow(dy, 2))
+            let dt = now.timeIntervalSince(lastAttempt.1)
+
+            let vSquared = magChange / CGFloat(dt)
+            print(vSquared)
+        }
+        lastAddAttempt = (position, now)
+    }
+
+    private func getTimeIntervalBetweenDrops(
+        startPosition: CGPoint,
+        currentTime: Date
+    ) -> CGFloat {
+        let velocity = getVelocity(currentPosition: startPosition, currentTime: currentTime)
+        self.lastAddAttempt = (startPosition, currentTime)
+        return AddingMoleculesSettings.getTimeInterval(velocity: velocity)
+    }
+
+    private func getVelocity(
+        currentPosition: CGPoint,
+        currentTime: Date
+    ) -> CGFloat? {
+        lastAddAttempt.map { lastAttempt in
+            let dx = currentPosition.x - lastAttempt.0.x
+            let dy = currentPosition.y - lastAttempt.0.y
+
+            let distance = sqrt(pow(dx, 2) + pow(dy, 2))
+            let dt = currentTime.timeIntervalSince(lastAttempt.1)
+
+            return distance / CGFloat(dt)
+        }
+    }
+
+
     private func moleculeHasHitWater(id: UUID) {
         if let index = molecules.firstIndex(where: { $0.id == id }) {
             molecules.remove(at: index)
         }
         addMolecules(1)
     }
+}
+
+private struct AddingMoleculesSettings {
+
+    static func getTimeInterval(velocity: CGFloat?) -> CGFloat {
+        velocity.map { v in
+            min(maxTimeInterval, max(minTimeInterval, equation.getY(at: v)))
+        } ?? maxTimeInterval
+    }
+
+    private static let minTimeInterval: CGFloat = 0.02
+    private static let maxTimeInterval: CGFloat = 0.2
+    private static let minVelocity: CGFloat = 100
+    private static let maxVelocity: CGFloat = 500
+
+    private static let equation = LinearEquation(
+        x1: minVelocity,
+        y1: maxTimeInterval,
+        x2: maxVelocity,
+        y2: minTimeInterval
+    )
+
 }
 
 struct FallingMolecule: Identifiable {
