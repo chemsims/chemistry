8a75c2a80a746ce41fcfa842603663902cbb1271
omar-f <o.fahmy@outlook.com>
2021-03-10 14:38:03 +0400

    EA-15: Updated colors of molecules A & B

--- reactions-equilibrium/model/styling/AqueousStyling.swift
+++ reactions-equilibrium/model/styling/AqueousStyling.swift
@@ -6,9 +6,8 @@
 import ReactionsCore
 
 extension RGB {
-    static let aqMoleculeA = RGB(r: 24, g: 146, b: 177)
-    static let aqMoleculeB = RGB(r: 172, g: 139, b: 20)
+    static let aqMoleculeA = RGB(r: 230, g: 175, b: 33)
+    static let aqMoleculeB = RGB(r: 9, g: 183, b: 99)
     static let aqMoleculeC = RGB(r: 214, g: 91, b: 35)
     static let aqMoleculeD = RGB(r: 28, g: 101, b: 185)
 }
-
