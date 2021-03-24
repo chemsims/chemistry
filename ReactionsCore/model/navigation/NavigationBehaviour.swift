a8e47a186dee7cb4940de3ff9ed0d908f49ea2ae
omar-f <o.fahmy@outlook.com>
2021-03-24 12:09:01 +0400

    EA-32: Added some more comments to the navigation model

--- ReactionsCore/model/navigation/NavigationBehaviour.swift
+++ ReactionsCore/model/navigation/NavigationBehaviour.swift
@@ -8,18 +8,28 @@ public protocol NavigationBehaviour {
 
     associatedtype Screen
 
-    /// Returns an alternative screen to check if it can be selected
-    ///
-    /// If the alternative screen can be selected, then the input screen can also be selected
+    /// Returns an alternative screen to check if the given screen can be opened
     func deferCanSelect(of screen: Screen) -> DeferCanSelect<Screen>?
 
     /// Returns whether the previous screen state should be restored when navigating to this screen from the menu
     func shouldRestoreStateWhenJumpingTo(screen: Screen) -> Bool
 
+    /// Returns whether the review prompt should be triggered when opening this screen.
+    ///
+    /// - Note:The review prompt is triggered after some delay, not immediately, and subject to
+    /// the conditions in the review prompting logic
     func showReviewPromptOn(screen: Screen) -> Bool
 
+    /// Returns the screen which should have it's navigation icon highlighted in the main menu
+    ///
+    /// - Note: The is different to a regular focused screen, and is designed to draw attention to a different
+    /// screen altogether in the main menu, using a different color. For example, the last screen on the app may
+    /// tell the user to visit a different screen next, in which case this screen could be highlighted in the menu
     func highlightedNavIcon(for screen: Screen) -> Screen?
 
+    /// Returns a screen provider for this screen
+    ///
+    /// Screen providers should maintain their underlying state, so that the same screen can be restored again later
     func getProvider(
         for screen: Screen,
         nextScreen: @escaping () -> Void,
--- ReactionsCore/model/navigation/ScreenProvider.swift
+++ ReactionsCore/model/navigation/ScreenProvider.swift
@@ -4,6 +4,12 @@
 
 import SwiftUI
 
+/// Provides the view for a screen of the app
+///
+/// The provider should maintain it's underlying state, so that the view can be restored again when
+/// viewing the screen later - for example, as the user goes back through the previous screens
+///
+/// It is not necessary to store the view itself in the provider - only the underlying state should be stored.
 public protocol ScreenProvider {
     var screen: AnyView { get }
 }
