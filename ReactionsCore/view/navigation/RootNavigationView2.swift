d6e90f991b15e0de4ce671c88db8b163f6845852
omar-f <o.fahmy@outlook.com>
2021-03-24 13:45:10 +0400

    EA-32: Added root navigation view to reactions core

---
+++ ReactionsCore/view/navigation/RootNavigationView2.swift
@@ -0,0 +1,62 @@
+//
+// Reactions App
+//
+
+import SwiftUI
+
+public struct RootNavigationView2<Injector: NavigationInjector>: View {
+
+    @ObservedObject var model: RootNavigationViewModel<Injector>
+    let navigationRows: [NavigationIconRow<Injector.Screen>]
+    let feedbackSettings: FeedbackSettings
+    let shareSettings: ShareSettings
+    let menuIconSize: CGFloat
+    let menuTopPadding: CGFloat
+    let menuHPadding: CGFloat
+
+    public init(
+        model: RootNavigationViewModel<Injector>,
+        navigationRows: [NavigationIconRow<Injector.Screen>],
+        feedbackSettings: FeedbackSettings,
+        shareSettings: ShareSettings,
+        menuIconSize: CGFloat,
+        menuTopPadding: CGFloat,
+        menuHPadding: CGFloat
+    ) {
+        self.model = model
+        self.navigationRows = navigationRows
+        self.feedbackSettings = feedbackSettings
+        self.shareSettings = shareSettings
+        self.menuIconSize = menuIconSize
+        self.menuTopPadding = menuTopPadding
+        self.menuHPadding = menuHPadding
+    }
+
+    public var body: some View {
+        ZStack {
+            model.view
+                .id(model.currentScreen)
+                .transition(
+                    .asymmetric(
+                        insertion: .move(
+                            edge: model.navigationDirection == .forward ? .trailing : .leading
+                        ),
+                        removal: AnyTransition.move(
+                            edge: model.navigationDirection == .forward ? .leading : .trailing
+                        ).combined(with: .opacity)
+                    )
+                )
+                .accessibility(hidden: model.showMenu)
+
+            MainMenuOverlay2(
+                rows: navigationRows,
+                navigation: model,
+                feedbackSettings: feedbackSettings,
+                shareSettings: shareSettings,
+                size: menuIconSize,
+                topPadding: menuTopPadding,
+                menuHPadding: menuHPadding
+            )
+        }
+    }
+}
