1b39d71afd9710fa405271d10b612d2284f13576
omar-f <o.fahmy@outlook.com>
2021-04-13 16:25:51 +0400

    RC: Added a particle container view to reactions core

---
+++ ReactionsCore/view/container/ParticleContainer.swift
@@ -0,0 +1,177 @@
+//
+// Reactions App
+//
+
+import SwiftUI
+
+public struct ParticleContainer: View {
+
+    let settings: ParticleContainerSettings
+
+    public init(settings: ParticleContainerSettings) {
+        self.settings = settings
+    }
+
+    public var body: some View {
+        GeometryReader { geo in
+            EmptyParticleContainer(
+                settings: settings,
+                geometry: ContainerGeometry(width: geo.size.width, height: geo.size.height)
+            )
+        }
+        .frame(idealWidth: 43, idealHeight: 100)
+        .aspectRatio(contentMode: .fit)
+
+    }
+}
+
+public struct ParticleContainerSettings {
+    let containerColor: Color
+    let labelColor: Color
+    let label: String?
+    let strokeColor: Color
+    let strokeLineWidth: CGFloat
+
+    public init(
+        containerColor: Color,
+        labelColor: Color,
+        label: String?,
+        strokeColor: Color = Color.black,
+        strokeLineWidth: CGFloat = 3
+    ) {
+        self.containerColor = containerColor
+        self.labelColor = labelColor
+        self.label = label
+        self.strokeColor = strokeColor
+        self.strokeLineWidth = strokeLineWidth
+    }
+}
+
+private struct EmptyParticleContainer: View {
+
+    let settings: ParticleContainerSettings
+    let geometry: ContainerGeometry
+
+    var body: some View {
+        GeometryReader { geo in
+            ZStack(alignment: .bottom) {
+                ContainerShape()
+                    .fill(settings.containerColor, style: FillStyle(eoFill: true, antialiased: true))
+
+                label
+
+                ContainerShape()
+                    .stroke(settings.strokeColor, lineWidth: settings.strokeLineWidth)
+            }
+        }
+    }
+
+    private var label: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(settings.labelColor)
+
+            if settings.label != nil {
+                Text(settings.label!)
+                    .fixedSize()
+                    .rotationEffect(.degrees(-90))
+            }
+        }
+        .frame(width: geometry.width, height: geometry.labelHeight)
+        .clipped()
+    }
+}
+
+private struct ContainerShape: Shape {
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let geometry = ContainerGeometry(
+            width: rect.width,
+            height: rect.height
+        )
+
+        addCurve(
+            to: &path, origin: .zero,
+            size: CGSize(
+                width: rect.width,
+                height: geometry.outerCapCurveHeight
+            )
+        )
+        path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+        path.addLine(to: CGPoint(x: 0, y: rect.height))
+        path.closeSubpath()
+
+        addCurve(
+            to: &path,
+            origin: CGPoint(
+                x: geometry.innerCapHorizontalPadding,
+                y: geometry.innerCapTopPadding
+            ),
+            size: CGSize(
+                width: geometry.innerCapWidth,
+                height: geometry.innerCapCurveHeight
+            )
+        )
+        path.closeSubpath()
+
+        return path
+    }
+
+    private func addCurve(to path: inout Path, origin: CGPoint, size: CGSize) {
+        path.move(to: CGPoint(x: origin.x, y: origin.y + size.height))
+        path.addQuadCurve(
+            to: CGPoint(x: origin.x + (size.width / 2), y: origin.y),
+            control: origin
+        )
+        path.addQuadCurve(
+            to: CGPoint(x: origin.x + size.width, y: origin.y + size.height),
+            control: CGPoint(x: origin.x + size.width, y: origin.y)
+        )
+    }
+}
+
+private struct ContainerGeometry {
+    let width: CGFloat
+    let height: CGFloat
+
+    var outerCapCurveHeight: CGFloat {
+        0.212 * height
+    }
+
+    var innerCapCurveHeight: CGFloat {
+        0.178 * height
+    }
+
+    var innerCapWidth: CGFloat {
+        width - (2 * innerCapHorizontalPadding)
+    }
+
+    var innerCapHorizontalPadding: CGFloat {
+        0.0832 * width
+    }
+
+    var innerCapTopPadding: CGFloat {
+        0.1018 * width
+    }
+
+    var labelHeight: CGFloat {
+        0.66 * height
+    }
+}
+
+struct ParticleContainer_Previews: PreviewProvider {
+    static var previews: some View {
+        ParticleContainer(
+            settings: ParticleContainerSettings(
+                containerColor: .white,
+                labelColor: .purple,
+                label: "AB"
+            )
+        )
+            .padding()
+            .font(.system(size: 300))
+            .foregroundColor(.white)
+    }
+}
