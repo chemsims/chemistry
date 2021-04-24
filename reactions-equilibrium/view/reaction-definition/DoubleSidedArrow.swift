6a6b8f7087ef22cc57880345fb5beefca9874d31
omar-f <o.fahmy@outlook.com>
2021-04-24 14:15:42 +0400

    EA-63: Added animating double sided arrow view

--- reactions-equilibrium/view/reaction-definition/DoubleSidedArrow.swift
+++ reactions-equilibrium/view/reaction-definition/DoubleSidedArrow.swift
@@ -5,6 +5,49 @@
 
 import SwiftUI
 
+struct AnimatingDoubleSidedArrow: View {
+
+    let topHighlight: Color?
+    let bottomHighlight: Color?
+    let width: CGFloat
+
+    @State private var offset: CGFloat = 0
+
+    var body: some View {
+        VStack(spacing: 2) {
+            imageStack
+                .foregroundColor(topHighlight ?? .black)
+                .offset(x: offset)
+            imageStack
+                .foregroundColor(bottomHighlight ?? .black)
+                .rotationEffect(.degrees(180))
+                .offset(x: -offset)
+        }
+        .frame(width: width)
+        .onAppear {
+            let animation = Animation.linear(duration: 0.75).repeatForever(autoreverses: false)
+            withAnimation(animation) {
+                offset = width
+            }
+        }
+        .clipped()
+    }
+
+    private var imageStack: some View {
+        HStack(spacing: 0) {
+            image
+            image
+        }
+        .offset(x: -width / 2)
+    }
+    private var image: some View {
+        Image("top-right-harpoon")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: width)
+    }
+}
+
 struct DoubleSidedArrow: View {
 
     let topHighlight: Color?
@@ -12,21 +55,39 @@ struct DoubleSidedArrow: View {
 
     var body: some View {
         VStack(spacing: 2) {
-            Image("top-right-harpoon")
+            image
                 .foregroundColor(topHighlight ?? .black)
-            Image("top-right-harpoon")
+            image
                 .foregroundColor(reverseHighlight ?? .black)
                 .rotationEffect(.degrees(180))
-
         }
+    }
+
+    private var image: some View {
+        Image("top-right-harpoon")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
     }
 }
 
 struct DoubleSidedArrow_Previews: PreviewProvider {
     static var previews: some View {
-        HStack {
-            Text("2A + 3B")
-            DoubleSidedArrow(topHighlight: nil, reverseHighlight: .orangeAccent)
+        VStack {
+            HStack {
+                Text("2A + 3B")
+                DoubleSidedArrow(topHighlight: nil, reverseHighlight: .orangeAccent)
+                    .frame(width: 40)
+            }
+            HStack {
+                Text("2A + 3B")
+                AnimatingDoubleSidedArrow(
+                    topHighlight: nil,
+                    bottomHighlight: .orangeAccent,
+                    width: 40
+                )
+                .frame(width: 40)
+            }
+
         }
         .font(.system(size: 30))
     }
