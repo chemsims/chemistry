fb74cf130cb6cb1deffea3839338ff1e0314459b
omar-f <o.fahmy@outlook.com>
2021-04-27 00:21:04 +0400

    EA-63: Updated molecule arc shape to animate molecule to allow changing the apex position, and reduce molecule count to 1 at the apex

--- ReactionsCore/view/util/shapes/MoleculeArc.swift
+++ ReactionsCore/view/util/shapes/MoleculeArc.swift
@@ -13,8 +13,9 @@ public struct MoleculeArc: Shape {
     /// - Parameters:
     ///     - verticalAlignment: Alignment of the start & end y positions
     ///     - horizontalAlignment:Alignment of the start & end x positions
-    ///     - finalCount: Number of molecules in the final state
-    ///     - finalRotation: Rotation of the molecules in the final state
+    ///     - startState: Molecule state at start position
+    ///     - endState: Molecule state at end position
+    ///     - apexXLocation: Position of the curve apex as a fraction of the width, between 0 and 1
     ///     - moleculeRadius: Radius of the molecule
     ///     - progress: Progress along the path, between 0 and 1
     public init(
@@ -22,6 +23,7 @@ public struct MoleculeArc: Shape {
         horizontalAlignment: HorizontalAlignment,
         startState: MoleculeArcState,
         endState: MoleculeArcState,
+        apexXLocation: CGFloat,
         moleculeRadius: CGFloat,
         progress: CGFloat
     ) {
@@ -31,6 +33,33 @@ public struct MoleculeArc: Shape {
         self.endState = endState
         self.moleculeRadius = moleculeRadius
         self.progress = progress.within(min: 0, max: 1)
+
+        self.yEquation = SwitchingEquation(
+            thresholdX: apexXLocation,
+            underlyingLeft: QuadraticEquation(
+                parabola: CGPoint(x: apexXLocation, y: 1),
+                through: .zero
+            ),
+            underlyingRight: QuadraticEquation(
+                parabola: CGPoint(x: apexXLocation, y: 1),
+                through: CGPoint(x: 1, y: 0)
+            )
+        )
+        self.xEquation = SwitchingEquation(
+            thresholdX: 0.5,
+            underlyingLeft: LinearEquation(
+                x1: 0,
+                y1: 0,
+                x2: 0.5,
+                y2: apexXLocation
+            ),
+            underlyingRight: LinearEquation(
+                x1: 0.5,
+                y1: apexXLocation,
+                x2: 1,
+                y2: 1
+            )
+        )
     }
 
     let vAlignment: VerticalAlignment
@@ -41,6 +70,9 @@ public struct MoleculeArc: Shape {
 
     var progress: CGFloat
 
+    private let yEquation: Equation
+    private let xEquation: Equation
+
     public var animatableData: CGFloat {
         get { progress }
         set { progress = newValue.within(min: 0, max: 1) }
@@ -48,19 +80,21 @@ public struct MoleculeArc: Shape {
 
     public func path(in rect: CGRect) -> Path {
         var path = Path()
+        let xFraction = xEquation.getY(at: progress)
         let x = AxisPositionCalculations(
             minValuePosition: hAlignment == .leading ? 0 : rect.width,
             maxValuePosition: hAlignment == .leading ? rect.width : 0,
             minValue: 0,
             maxValue: 1
-        ).getPosition(at: progress)
+        ).getPosition(at: xFraction)
 
+        let yFraction = yEquation.getY(at: xFraction)
         let y = AxisPositionCalculations<CGFloat>(
             minValuePosition: vAlignment == .bottom ? rect.height : 0,
             maxValuePosition: vAlignment == .bottom ? 0 : rect.height,
             minValue: 0,
             maxValue: 1
-        ).getPosition(at: Self.equation.getY(at: progress))
+        ).getPosition(at: yFraction)
 
         addMolecules(at: CGPoint(x: x, y: y), path: &path)
         return path
@@ -94,10 +128,34 @@ public struct MoleculeArc: Shape {
     }
 
     private func getScaledOffset(forIndex index: Int) -> CGSize {
-        let startOffset = getMaxOffset(forIndex: index, count: startState.count)
-        let endOffset = getMaxOffset(forIndex: index, count: endState.count)
+        if progress < 0.5 {
+            return getInterpolatedOffset(
+                moleculeIndex: index,
+                startCount: startState.count,
+                endCount: .one,
+                fraction: 2 * progress
+            )
+        }
+        return getInterpolatedOffset(
+            moleculeIndex: index,
+            startCount: .one,
+            endCount: endState.count,
+            fraction: 2 * (progress - 0.5)
+        )
+    }
+
+    private func getInterpolatedOffset(
+        moleculeIndex: Int,
+        startCount: Count,
+        endCount: Count,
+        fraction: CGFloat
+    ) -> CGSize {
+        let startOffset =
+            getMaxOffset(forIndex: moleculeIndex, count: startCount)
+        let endOffset =
+            getMaxOffset(forIndex: moleculeIndex, count: endCount)
         let deltaOffset = endOffset - startOffset
-        return startOffset + deltaOffset.scale(by: progress)
+        return startOffset + deltaOffset.scale(by: fraction)
     }
 
     private func getMaxOffset(
@@ -111,11 +169,6 @@ public struct MoleculeArc: Shape {
         case .four: return offsetForCountOfFour(forIndex: index)
         }
     }
-
-    private static let equation = QuadraticEquation(
-        parabola: CGPoint(x: 0.5, y: 1),
-        through: CGPoint(x: 0, y: 0)
-    )
 }
 
 //MARK: Enums
@@ -237,7 +290,6 @@ struct MoleculeArc_Previews: PreviewProvider {
             ViewWrapper(vAlignment: .bottom, hAlignment: .leading)
             ViewWrapper(vAlignment: .top, hAlignment: .trailing)
         }
-
     }
 
     struct ViewWrapper: View {
@@ -253,15 +305,16 @@ struct MoleculeArc_Previews: PreviewProvider {
                 horizontalAlignment: hAlignment,
                 startState: MoleculeArcState(count: .three, rotation: .degrees(20)),
                 endState: MoleculeArcState(count: .four, rotation: .degrees(-45)),
+                apexXLocation: 0.3,
                 moleculeRadius: 15,
                 progress: progress
             )
             .frame(width: 200, height: 100)
             .border(Color.red)
             .onAppear {
-                let animation = Animation.easeOut(duration: 0.75).repeatForever()
+                let animation = Animation.easeOut(duration: 2).repeatForever()
                 withAnimation(animation) {
-                    progress = 0
+                    progress = 1
                 }
             }
         }
