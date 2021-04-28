6202b5cd23322e01c031de6649895be7097152a3
omar-f <o.fahmy@outlook.com>
2021-04-28 17:23:32 +0400

    EA-65: Added a function to return unique coords given a list of list of coords

---
+++ ReactionsCore/model/grid/UniqueGridCoordinates.swift
@@ -0,0 +1,18 @@
+//
+// Reactions App
+//
+
+
+import Foundation
+
+extension GridCoordinate {
+
+    /// Returns an array of grid coordinates where all coordinates are unique
+    ///
+    /// In the case of a duplicate, the first occurring coordinate is kept, while any others are returned
+    static func uniqueGridCoordinates(
+        coords: [[GridCoordinate]]
+    ) -> [[GridCoordinate]] {
+        return coords
+    }
+}
