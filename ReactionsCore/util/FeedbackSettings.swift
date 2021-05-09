4e35dfc699040b27c7914408606db1b2c9280e86
omar-f <o.fahmy@outlook.com>
2021-05-09 21:05:54 +0400

    RC: Moved feedback settings into its own file

---
+++ ReactionsCore/util/FeedbackSettings.swift
@@ -0,0 +1,60 @@
+//
+// Reactions App
+//
+
+import Foundation
+
+public struct FeedbackSettings {
+    let toAddress: String
+    let subject: String
+    let body: String
+
+    public init(
+        toAddress: String,
+        subject: String,
+        body: String
+    ) {
+        self.toAddress = toAddress
+        self.subject = subject
+        self.body = body
+    }
+
+    public init(
+        toAddress: String,
+        subject: String
+    ) {
+        self.init(
+            toAddress: toAddress,
+            subject: subject,
+            body: Self.defaultBody
+        )
+    }
+
+    public init(appName: String) {
+        self.init(
+            toAddress: Self.defaultAddress,
+            subject: "\(appName) Feedback",
+            body: Self.defaultBody
+        )
+    }
+
+    public static let defaultAddress = "support@reactionsrateapp.com"
+    private static let defaultBody = """
+
+    ---------
+    Thank you for taking the time to send your feedback. Please add your comments above.
+    \(getVersion ?? "")
+    """
+
+    private static let getVersion = Version.getCurrentVersion().map { v in
+        "Version: \(v)"
+    }
+
+    var mailToUrl: URL? {
+        if let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+           let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            return URL(string: "mailto:\(toAddress)?subject=\(encodedSubject)&body=\(encodedBody)")
+        }
+        return nil
+    }
+}
