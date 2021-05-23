9470a9011fb3ad80edd12a292a4e63de16c56a09
omar-f <o.fahmy@outlook.com>
2021-05-23 15:58:18 +0400

    AB: Added acid & bases project

---
+++ acids-bases/ContentView.swift
@@ -0,0 +1,19 @@
+//
+// Reactions App
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
