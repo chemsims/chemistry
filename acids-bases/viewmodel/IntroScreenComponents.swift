8e5037e0d8bb33e38a2ae7d8d9beb7ca21f1ac02
omar-f <o.fahmy@outlook.com>
2021-05-23 15:58:34 +0400

    AB: Added intro screen components protocol

--- acids-bases/viewmodel/IntroScreenComponents.swift
+++ acids-bases/viewmodel/IntroScreenComponents.swift
@@ -2,5 +2,38 @@
 // Reactions App
 //
 
+import CoreGraphics
+import ReactionsCore
 
-import Foundation
+protocol IntroScreenComponents {
+    func increment(substance: Substance, count: Int)
+
+    func concentration(ofIon ion: PrimaryIon) -> PrimaryIonConcentration
+
+    var fractionSubstanceAdded: CGFloat { get }
+
+    var coords: [BeakerMolecules] { get }
+
+    var barChart: [BarChartData] { get }
+}
+
+struct PrimaryIonConcentration {
+    let concentration: CGFloat
+    let p: CGFloat
+}
+
+struct Substance: Equatable {
+    let name: String
+    let primary: PrimaryIon
+    let secondary: SecondaryIon
+}
+
+enum PrimaryIon {
+    case hydrogen
+    case hydroxide
+}
+
+enum SecondaryIon {
+    case A
+    case Cl
+}
