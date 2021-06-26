7973def4d1c4136a55fc0e1b9ff3e63ce8672fa3
Omar <o.fahmy@outlook.com>
2021-06-26 09:29:03 +0400

    RC: Fix tests for reaction progress chart geometry since the column spacing was updated

--- ReactionsCoreTests/reaction-progress/ReactionProgressChartGeometryTests.swift
+++ ReactionsCoreTests/reaction-progress/ReactionProgressChartGeometryTests.swift
@@ -15,20 +15,26 @@ class ReactionProgressChartGeometryTests: XCTestCase {
             topPadding: 100
         )
 
+        let barWidth = 500 / 4.0
+        let firstColX = barWidth / 2
+        let expectedColX = [firstColX, firstColX + barWidth, firstColX + (2 * barWidth), firstColX + (3 * barWidth)]
+
+        // Available height is 400 for 10 molecules, so expected molecule size is 40
         XCTAssertEqual(model.moleculeSize, 40)
-        XCTAssertEqual(model.position(colIndex: 0, rowIndex: 0), CGPoint(x: 100, y: 480))
-        XCTAssertEqual(model.position(colIndex: 1, rowIndex: 0), CGPoint(x: 200, y: 480))
-        XCTAssertEqual(model.position(colIndex: 2, rowIndex: 0), CGPoint(x: 300, y: 480))
-        XCTAssertEqual(model.position(colIndex: 3, rowIndex: 0), CGPoint(x: 400, y: 480))
 
-        XCTAssertEqual(model.position(colIndex: 0, rowIndex: 1), CGPoint(x: 100, y: 440))
-        XCTAssertEqual(model.position(colIndex: 1, rowIndex: 1), CGPoint(x: 200, y: 440))
-        XCTAssertEqual(model.position(colIndex: 2, rowIndex: 1), CGPoint(x: 300, y: 440))
-        XCTAssertEqual(model.position(colIndex: 3, rowIndex: 1), CGPoint(x: 400, y: 440))
+        XCTAssertEqual(model.position(colIndex: 0, rowIndex: 0), CGPoint(x: expectedColX[0], y: 480))
+        XCTAssertEqual(model.position(colIndex: 1, rowIndex: 0), CGPoint(x: expectedColX[1], y: 480))
+        XCTAssertEqual(model.position(colIndex: 2, rowIndex: 0), CGPoint(x: expectedColX[2], y: 480))
+        XCTAssertEqual(model.position(colIndex: 3, rowIndex: 0), CGPoint(x: expectedColX[3], y: 480))
 
-        XCTAssertEqual(model.position(colIndex: 0, rowIndex: 9), CGPoint(x: 100, y: 120))
-        XCTAssertEqual(model.position(colIndex: 1, rowIndex: 9), CGPoint(x: 200, y: 120))
-        XCTAssertEqual(model.position(colIndex: 2, rowIndex: 9), CGPoint(x: 300, y: 120))
-        XCTAssertEqual(model.position(colIndex: 3, rowIndex: 9), CGPoint(x: 400, y: 120))
+        XCTAssertEqual(model.position(colIndex: 0, rowIndex: 1), CGPoint(x: expectedColX[0], y: 440))
+        XCTAssertEqual(model.position(colIndex: 1, rowIndex: 1), CGPoint(x: expectedColX[1], y: 440))
+        XCTAssertEqual(model.position(colIndex: 2, rowIndex: 1), CGPoint(x: expectedColX[2], y: 440))
+        XCTAssertEqual(model.position(colIndex: 3, rowIndex: 1), CGPoint(x: expectedColX[3], y: 440))
+
+        XCTAssertEqual(model.position(colIndex: 0, rowIndex: 9), CGPoint(x: expectedColX[0], y: 120))
+        XCTAssertEqual(model.position(colIndex: 1, rowIndex: 9), CGPoint(x: expectedColX[1], y: 120))
+        XCTAssertEqual(model.position(colIndex: 2, rowIndex: 9), CGPoint(x: expectedColX[2], y: 120))
+        XCTAssertEqual(model.position(colIndex: 3, rowIndex: 9), CGPoint(x: expectedColX[3], y: 120))
     }
 }
