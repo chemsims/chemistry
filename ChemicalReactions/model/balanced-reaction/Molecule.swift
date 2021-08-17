b170be132c79808fc8564a1d05f08e5fef5e218a
Omar <o.fahmy@outlook.com>
2021-08-17 22:45:59 +0200

    CR: balanced reaction: call the appropriate molecule structure views in the parent molecule view

--- ChemicalReactions/model/balanced-reaction/Molecule.swift
+++ ChemicalReactions/model/balanced-reaction/Molecule.swift
@@ -16,6 +16,14 @@ extension BalancedReaction {
              nitriteIon
     }
 
+    enum MoleculeStructure {
+        case double(atom: BalancedReaction.Atom),
+             oneToTwoHorizontal(singleAtom: BalancedReaction.Atom, doubleAtom: BalancedReaction.Atom),
+             oneToTwoPyramid(singleAtom: BalancedReaction.Atom, doubleAtom: BalancedReaction.Atom),
+             oneToThree(singleAtom: BalancedReaction.Atom, tripleAtom: BalancedReaction.Atom),
+             oneToFour(singleAtom: BalancedReaction.Atom, quadAtom: BalancedReaction.Atom)
+    }
+
     struct MoleculeCount {
         init(molecule: BalancedReaction.Molecule, coefficient: Int) {
             assert(coefficient >= 1)
@@ -26,6 +34,40 @@ extension BalancedReaction {
         let molecule: BalancedReaction.Molecule
         let coefficient: Int
     }
+}
+
+extension BalancedReaction.MoleculeStructure {
+
+    static let ammonia = BalancedReaction.MoleculeStructure.oneToThree(
+        singleAtom: .nitrogen,
+        tripleAtom: .hydrogen
+    )
+
+    static let carbonDioxide = BalancedReaction.MoleculeStructure.oneToTwoHorizontal(
+        singleAtom: .carbon,
+        doubleAtom: .oxygen
+    )
+
+    static let dinitrogen = BalancedReaction.MoleculeStructure.double(atom: .nitrogen)
+
+    static let dihydrogen = BalancedReaction.MoleculeStructure.double(atom: .hydrogen)
+
+    static let dioxygen = BalancedReaction.MoleculeStructure.double(atom: .oxygen)
+
+    static let methane = BalancedReaction.MoleculeStructure.oneToFour(
+        singleAtom: .carbon,
+        quadAtom: .hydrogen
+    )
+
+    static let nitriteIon = BalancedReaction.MoleculeStructure.oneToTwoHorizontal(
+        singleAtom:  .nitrogen,
+        doubleAtom: .oxygen
+    )
+
+    static let water = BalancedReaction.MoleculeStructure.oneToTwoPyramid(
+        singleAtom: .oxygen,
+        doubleAtom: .hydrogen
+    )
 }
 
 extension BalancedReaction.Molecule {
@@ -61,5 +103,9 @@ extension BalancedReaction.Molecule {
             .init(atom: .oxygen, count: 2)
         ]
         }
+    }
+
+    func atom(withCount count: Int) -> BalancedReaction.Atom? {
+        atoms.first { $0.count == count }?.atom
     }
 }
--- ChemicalReactions/view/balanced-reaction/molecules/BalancedReactionMoleculesView.swift
+++ ChemicalReactions/view/balanced-reaction/molecules/BalancedReactionMoleculesView.swift
@@ -2,12 +2,82 @@
 // Reactions App
 //
 
-
 import SwiftUI
 
 struct BalancedReactionMoleculeView: View {
+
+    let structure: BalancedReaction.MoleculeStructure
+    let atomSize: CGFloat
+
+    @ViewBuilder
     var body: some View {
-        Text("Hello, world")
+        switch structure {
+        case let .oneToFour(single, quad):
+            oneToFour(single: single, quad: quad)
+
+        case let .double(atom):
+            double(atom: atom)
+
+        case let .oneToThree(single, triple):
+            oneToThree(single: single, triple: triple)
+
+        case let .oneToTwoHorizontal(single, double):
+            oneToTwoHorizontal(single: single, double: double)
+
+        case let .oneToTwoPyramid(single, double):
+            oneToTwoPyramid(single: single, double: double)
+        }
+    }
+
+    private func double(atom: BalancedReaction.Atom) -> some View {
+        BalancedReactionMoleculeView.Double(
+            atomSize: atomSize,
+            atom: atom
+        )
+    }
+
+    private func oneToTwoHorizontal(
+        single: BalancedReaction.Atom,
+        double: BalancedReaction.Atom
+    ) -> some View {
+        BalancedReactionMoleculeView.OneToTwoHorizontal(
+            atomSize: atomSize,
+            singleAtom: single,
+            doubleAtom: double
+        )
+    }
+
+    private func oneToTwoPyramid(
+        single: BalancedReaction.Atom,
+        double: BalancedReaction.Atom
+    ) -> some View {
+        BalancedReactionMoleculeView.OneToTwoPyramid(
+            atomSize: atomSize,
+            singleAtom: single,
+            doubleAtom: double
+        )
+    }
+
+    private func oneToThree(
+        single: BalancedReaction.Atom,
+        triple: BalancedReaction.Atom
+    ) -> some View {
+        BalancedReactionMoleculeView.OneToThree(
+            atomSize: atomSize,
+            singleAtom: single,
+            tripleAtom: triple
+        )
+    }
+
+    private func oneToFour(
+        single: BalancedReaction.Atom,
+        quad: BalancedReaction.Atom
+    ) -> some View {
+        BalancedReactionMoleculeView.OneToFour(
+            atomSize: atomSize,
+            singleAtom: single,
+            quadAtom: quad
+        )
     }
 }
 
@@ -32,6 +102,9 @@ extension BalancedReactionMoleculeView {
 
 struct BalancedReactionMoleculesView_Previews: PreviewProvider {
     static var previews: some View {
-        BalancedReactionMoleculeView()
+        BalancedReactionMoleculeView(
+            structure: .water,
+            atomSize: 100
+        )
     }
 }
