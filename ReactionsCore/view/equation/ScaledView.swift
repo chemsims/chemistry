05459a837a76788715f377e2e321b7b7b7a7f1b5
Omar Fahmy <o.fahmy@outlook.com>
2022-01-16 20:09:18 +0400

    Moved ScaledViewModifier to CogSciKit

--- ReactionsCore/view/equation/ScaledView.swift
+++ ReactionsCore/view/equation/ScaledView.swift
@@ -37,36 +37,3 @@ public struct ScaledView<Content: View>: View {
         return min(xScale, yScale)
     }
 }
-
-extension View {
-    public func scaledToFit(
-        inWidth width: CGFloat?,
-        inHeight height: CGFloat?,
-        naturalSize: CGSize
-    ) -> some View {
-        self.modifier(ScaledViewModifier(
-            naturalWidth: naturalSize.width,
-            naturalHeight: naturalSize.height,
-            maxWidth: width,
-            maxHeight: height
-        ))
-    }
-}
-
-private struct ScaledViewModifier: ViewModifier {
-    let naturalWidth: CGFloat
-    let naturalHeight: CGFloat
-    let maxWidth: CGFloat?
-    let maxHeight: CGFloat?
-
-    func body(content: Content) -> some View {
-        content
-            .scaleEffect(x: scale, y: scale)
-    }
-
-    private var scale: CGFloat {
-        let xScale = maxWidth.map { $0 / naturalWidth } ?? 1
-        let yScale = maxHeight.map { $0 / naturalHeight } ?? 1
-        return min(xScale, yScale)
-    }
-}
