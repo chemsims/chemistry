84fe3edf88d8d0b68f6fea1c9d8099d91ca1aadb
Omar Fahmy <o.fahmy@outlook.com>
2022-01-16 17:48:19 +0400

    Moved time chart data line views to CogSciKit

--- ReactionsCore/model/layout-settings/TimeChartLayoutSettings.swift
+++ ReactionsCore/model/layout-settings/TimeChartLayoutSettings.swift
@@ -4,25 +4,6 @@
 
 import CoreGraphics
 
-public struct TimeChartLayoutSettings {
-    public let xAxis: LinearAxis<CGFloat>
-    public let yAxis: LinearAxis<CGFloat>
-    public let haloRadius: CGFloat
-    public let lineWidth: CGFloat
-
-    public init(
-        xAxis: LinearAxis<CGFloat>,
-        yAxis: LinearAxis<CGFloat>,
-        haloRadius: CGFloat,
-        lineWidth: CGFloat
-    ) {
-        self.xAxis = xAxis
-        self.yAxis = yAxis
-        self.haloRadius = haloRadius
-        self.lineWidth = lineWidth
-    }
-}
-
 public struct ChartAxisShapeSettings {
     public let verticalTicks: Int
     public let horizontalTicks: Int
